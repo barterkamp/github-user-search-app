e75ea6aca690d10bdae17fcb19d7b80b8765b257
Bart Erkamp <bart.erkamp@maxserv.com>
2024-07-26 15:19:29 +0200

    feat: add searchInput and searchButton components

--- src/components/Search/SearchButton.jsx
+++ src/components/Search/SearchButton.jsx
@@ -1,6 +1,6 @@
 function SearchButton({ children }) {
   return (
-    <button className="absolute right-2.5 top-1/2 -translate-y-1/2 transform cursor-pointer rounded-xl bg-azure px-[18px] py-3 font-spaceMono text-[14px] font-bold text-white hover:bg-blueJeans md:px-6 md:text-lg">
+    <button className="absolute right-2.5 top-1/2 -translate-y-1/2 transform cursor-pointer rounded-xl bg-azure px-[18px] py-3 text-[14px] font-bold text-white hover:bg-blueJeans md:px-6 md:text-lg">
       {children}
     </button>
   );
--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react';
 import iconSearch from '../../assets/icons/icon-search.svg';
 import SearchButton from './SearchButton.jsx';
 
-function SearchInput() {
-  const [searchTerm, setSearchTerm] = useState('');
-  function handleSearch(e) {
+function SearchInput({ searchInput, setSearchInput, setSearchQuery, darkMode }) {
+  function handleSearchInput(e) {
     e.preventDefault();
-    setSearchTerm(e.target.value);
+    setSearchInput(e.target.value);
   }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setSearchQuery(searchInput);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <label htmlFor="search-input" className="relative">
         <input
-          className="shadow-custom mb-4 w-full rounded-[15px] bg-offWhite py-4 pl-8 font-spaceMono text-sm text-charlestonGreen md:mb-6 md:py-5 md:pl-20 md:text-[18px]"
+          className={`${darkMode ? 'bg-yankeesBlue text-white placeholder-white shadow-custom' : 'bg-offWhite text-charlestonGreen'} mb-4 w-full rounded-[15px] py-4 pl-10 text-sm transition-all duration-300 md:mb-6 md:py-5 md:pl-20 md:text-[18px]`}
           type="text"
           id="search-input"
-          value={searchTerm}
-          onChange={handleSearch}
-          placeholder="Search Github username.."
+          value={searchInput}
+          onChange={handleSearchInput}
+          placeholder="Search username.."
         />
         <img
           src={iconSearch}
           alt="search"
-          className="absolute left-2.5 md:left-8 top-1/2 size-5 flex-shrink-0 -translate-y-1/2 transform md:left-4"
+          className="absolute left-2.5 top-1/2 size-5 flex-shrink-0 -translate-y-1/2 transform md:left-4 md:left-8"
         />
         <SearchButton>Search</SearchButton>
       </label>
