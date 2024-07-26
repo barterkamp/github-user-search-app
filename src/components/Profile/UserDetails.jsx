6dffd5da78d0606e157416b8c1991526d5cfbc62
Bart Erkamp <bart.erkamp@maxserv.com>
2024-07-26 15:53:03 +0200

    feat: change gap sizes

--- src/components/Profile/UserDetails.jsx
+++ src/components/Profile/UserDetails.jsx
@@ -15,11 +15,11 @@ function getFillClass(darkMode, value) {
 function UserDetails({ city, url, twitter, company, darkMode }) {
   return (
     <ul className="flex flex-col gap-y-4">
-      <li className="flex gap-x-7">
+      <li className="flex shrink-0 gap-x-3">
         <svg
           className={`${getFillClass(darkMode, city)} h-5`}
           height="20"
-          width="14"
+          width="20"
           xmlns="http://www.w3.org/2000/svg">
           <path
             d="M12.797 3.425C11.584 1.33 9.427.05 7.03.002a7.483 7.483 0 00-.308 0C4.325.05 2.17 1.33.955 3.425a6.963 6.963 0 00-.09 6.88l4.959 9.077.007.012c.218.38.609.606 1.045.606.437 0 .828-.226 1.046-.606l.007-.012 4.96-9.077a6.963 6.963 0 00-.092-6.88zm-5.92 5.638c-1.552 0-2.813-1.262-2.813-2.813s1.261-2.812 2.812-2.812S9.69 4.699 9.69 6.25 8.427 9.063 6.876 9.063z"
@@ -31,7 +31,7 @@ function UserDetails({ city, url, twitter, company, darkMode }) {
         </span>
       </li>
 
-      <li className="flex gap-x-5">
+      <li className="flex shrink-0 gap-x-3">
         <svg
           className={`${getFillClass(darkMode, url)} h-5`}
           height="20"
@@ -48,10 +48,10 @@ function UserDetails({ city, url, twitter, company, darkMode }) {
         </span>
       </li>
 
-      <li className="flex gap-x-5">
+      <li className="flex shrink-0 gap-x-3">
         <svg
           className={`${getFillClass(darkMode, twitter)} h-5`}
-          height="18"
+          height="20"
           width="20"
           xmlns="http://www.w3.org/2000/svg">
           <path
@@ -65,7 +65,7 @@ function UserDetails({ city, url, twitter, company, darkMode }) {
         </span>
       </li>
 
-      <li className="flex gap-x-5">
+      <li className="flex shrink-0 gap-x-3">
         <svg
           className={`${getFillClass(darkMode, twitter)} h-5`}
           id="company"
