aeb6407c675ee75537e4495043baf97e07e5a89d
Bart Erkamp <bart.erkamp@maxserv.com>
2024-08-02 16:04:25 +0200

    feat: add text sizes for medium screens and above

--- src/components/Profile/Statistics/Stats.jsx
+++ src/components/Profile/Statistics/Stats.jsx
@@ -1,8 +1,11 @@
 function Stats({ title, value, darkMode }) {
   return (
     <div className="flex flex-col items-center gap-1">
-      <h2 className={`${darkMode ? 'text-white' : 'text-queenBlue'} text-sm`}>{title}</h2>
-      <p className={`${darkMode ? 'text-white' : 'text-gunMetal'} text-lg font-bold uppercase`}>
+      <h2 className={`${darkMode ? 'text-white' : 'text-queenBlue'} text-[11px] md:text-sm`}>
+        {title}
+      </h2>
+      <p
+        className={`${darkMode ? 'text-white' : 'text-gunMetal'} text-lg font-bold uppercase md:text-xl`}>
         {value}
       </p>
     </div>
