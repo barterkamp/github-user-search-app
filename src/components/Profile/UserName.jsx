e0172705c1f6b42289889e7e68d311a44430505a
Bart Erkamp <bart.erkamp@maxserv.com>
2024-08-02 16:07:33 +0200

    feat: add welcome message and inputError components

--- src/components/Profile/UserName.jsx
+++ src/components/Profile/UserName.jsx
@@ -10,17 +10,21 @@ function UserName({ avatar, name, username, date, darkMode }) {
   const formattedDate = handleFormatDate(date);
 
   return (
-    <section className="mb-8 flex gap-5">
-      <div className="shrink-0">
-        <img src={avatar} alt="avatar" className="size-[70px] rounded-full" />
+    <section className="mb-8 flex items-center gap-x-5 md:gap-x-10 lg:mb-0 lg:items-start lg:pl-12 lg:pr-12">
+      <div className="shrink-0 lg:absolute">
+        <img src={avatar} alt="avatar" className="block w-[70px] rounded-full md:w-[117px]" />
       </div>
-      <div>
-        <h1 className={`${darkMode ? 'text-white' : 'text-gunMetal'} text-lg font-bold`}>
+      <div className="flex w-full flex-col md:gap-1.5 lg:relative lg:pl-[158px]">
+        <h1
+          className={`${darkMode ? 'text-white' : 'text-gunMetal'} text-lg font-bold md:text-2xl`}>
           {name ?? `Not Available`}
         </h1>
-        <p className="text-sm text-azure">{username ? `@${username}` : `Not Available`}</p>
-        <span className={`${darkMode ? 'text-white' : 'text-gunMetal'} text-sm`}>
-          {formattedDate ? `${formattedDate}` : `Not Available`}
+        <p className="text-sm text-azure md:text-lg lg:mb-5">
+          {username ? `@${username}` : `Not Available`}
+        </p>
+        <span
+          className={`${darkMode ? 'text-white' : 'text-slateGray'} text-sm md:text-base lg:absolute lg:right-0 lg:top-2`}>
+          {formattedDate ? `Joined ${formattedDate}` : `Not Available`}
         </span>
       </div>
     </section>
--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -4,9 +4,10 @@ import UserStatistics from './Statistics/UserStatistics.jsx';
 import UserDetails from './UserDetails.jsx';
 import Loader from '../Loader.jsx';
 import ErrorMessage from '../ErrorMessage.jsx';
-import Introduction from '../Introduction.jsx';
+import WelcomeMessage from '../WelcomeMessage.jsx';
+import InputError from '../InputError.jsx';
 
-function UserProfile({ profileData, isLoading, error, darkMode }) {
+function UserProfile({ profileData, isLoading, error, darkMode, welcomeMessage, inputError }) {
   const {
     avatar_url,
     name,
@@ -26,9 +27,9 @@ function UserProfile({ profileData, isLoading, error, darkMode }) {
 
   return (
     <section
-      className={`${darkMode ? 'bg-yankeesBlue' : 'bg-offWhite shadow-custom'} relative min-h-8 rounded-[15px] px-6 py-8 transition-all duration-200`}>
+      className={`${darkMode ? 'bg-yankeesBlue' : 'bg-offWhite shadow-custom'} relative min-h-8 rounded-[15px] px-6 py-8 transition-all duration-200 md:p-10 lg:px-0 lg:py-11`}>
       {/* if there is an error, show the error*/}
-      {error && <ErrorMessage message={error} />}
+      {error && <ErrorMessage message={error} darkMode={darkMode} />}
 
       {/* if the data is loading, show the loader */}
       {isLoading && (
@@ -37,8 +38,15 @@ function UserProfile({ profileData, isLoading, error, darkMode }) {
         </div>
       )}
 
-      {/* if there is no data, no error and the loading state is false, show the welcome message */}
-      {!isLoading && !hasProfileData && !error && <Introduction darkMode={darkMode} />}
+      {/* if there is no data, no error, the loading state is false and there is no input error, show the welcome message */}
+      {!isLoading && !hasProfileData && !error && !inputError && welcomeMessage && (
+        <WelcomeMessage darkMode={darkMode} />
+      )}
+
+      {/* if there is no data, no error, the loading state is false and there is an input error, show the input error message */}
+      {!isLoading && !hasProfileData && !error && !welcomeMessage && inputError && (
+        <InputError darkMode={darkMode} />
+      )}
 
       {/* if there is data, no error and the loading state is false, show the user profile */}
       {!isLoading && !error && hasProfileData && (
@@ -50,20 +58,22 @@ function UserProfile({ profileData, isLoading, error, darkMode }) {
             date={created_at}
             darkMode={darkMode}
           />
-          <UserBio bio={bio} darkMode={darkMode} />
-          <UserStatistics
-            repos={public_repos}
-            followers={followers}
-            following={following}
-            darkMode={darkMode}
-          />
-          <UserDetails
-            city={location}
-            url={html_url}
-            twitter={twitter_username}
-            company={company}
-            darkMode={darkMode}
-          />
+          <section className="lg:pl-[206px] lg:pr-12">
+            <UserBio bio={bio} darkMode={darkMode} />
+            <UserStatistics
+              repos={public_repos}
+              followers={followers}
+              following={following}
+              darkMode={darkMode}
+            />
+            <UserDetails
+              city={location}
+              url={html_url}
+              twitter={twitter_username}
+              company={company}
+              darkMode={darkMode}
+            />
+          </section>
         </>
       )}
     </section>
