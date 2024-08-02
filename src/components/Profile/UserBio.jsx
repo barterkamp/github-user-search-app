23057a1214a89b50cd414eff444a27613bfd5014
Bart Erkamp <bart.erkamp@maxserv.com>
2024-08-02 16:05:29 +0200

    feat: add text sizes for medium screens and above and change text when a bio isn't found

--- src/components/Profile/UserBio.jsx
+++ src/components/Profile/UserBio.jsx
@@ -1,7 +1,8 @@
 function UserBio({ bio, darkMode }) {
   return (
-    <p className={`${darkMode ? 'text-white' : 'text-queenBlue'} mb-6 text-sm text-queenBlue`}>
-      {bio ?? 'No bio available'}
+    <p
+      className={`${darkMode ? 'text-white' : 'text-queenBlue'} mb-6 text-sm text-queenBlue md:text-base`}>
+      {bio ?? 'This profile has no bio'}
     </p>
   );
 }
