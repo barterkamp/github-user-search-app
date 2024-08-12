da40841ce63b217d6f294f026353c1c23c41c659
Bart Erkamp <bart.erkamp@maxserv.com>
2024-08-12 17:46:15 +0200

    feat: add section for better semantics

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -32,25 +32,27 @@ function SearchInput({
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <label htmlFor="search-input" className="relative">
-        <input
-          className={`${darkMode ? 'bg-yankeesBlue text-white placeholder-white shadow-custom' : 'bg-offWhite text-charlestonGreen'} mb-4 w-full rounded-[15px] py-4 pl-10 text-lg transition-all duration-200 md:mb-6 md:py-5 md:pl-20 md:text-[18px]`}
-          type="text"
-          id="search-input"
-          ref={inputRef}
-          value={searchInput}
-          onChange={handleSearchInput}
-          placeholder="Search username.."
-        />
-        <img
-          src={iconSearch}
-          alt="search"
-          className="absolute left-2.5 top-1/2 size-5 flex-shrink-0 -translate-y-1/2 transform md:left-4 md:left-8"
-        />
-        <SearchButton>Search</SearchButton>
-      </label>
-    </form>
+    <section>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="search-input" className="relative">
+          <input
+            className={`${darkMode ? 'bg-yankeesBlue text-white placeholder-white shadow-custom' : 'bg-offWhite text-charlestonGreen'} mb-4 w-full rounded-[15px] py-4 pl-10 text-lg transition-all duration-200 md:mb-6 md:py-5 md:pl-20 md:text-[18px]`}
+            type="text"
+            id="search-input"
+            ref={inputRef}
+            value={searchInput}
+            onChange={handleSearchInput}
+            placeholder="Search username.."
+          />
+          <img
+            src={iconSearch}
+            alt="search"
+            className="absolute left-2.5 top-1/2 size-5 flex-shrink-0 -translate-y-1/2 transform md:left-4 md:left-8"
+          />
+          <SearchButton>Search</SearchButton>
+        </label>
+      </form>
+    </section>
   );
 }
 
